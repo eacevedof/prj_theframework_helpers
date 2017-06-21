9561d9d164899b5c773af907c5e3ed858c5c7b1d
eacevedof <eacevedof@yahoo.es>
2017-06-21 10:33:07 +0200

    ModelUrlSeo

---
+++ the_application/models/ModelUrlSeo.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link www.eduardoaf.com
+ * @name ModelUrlSeo
+ * @file model_url_seo.php 
+ * @version 1.0.0
+ * @date 29-04-20170426 08:41 (SPAIN)
+ * @observations:
+ * @requires
+ */
+namespace TheApplication\Models;
+
+class ModelUrlSeo
+{
+    public function __construct() {
+        ;
+    }
+    
+    
+}//ModelUrlSeo
