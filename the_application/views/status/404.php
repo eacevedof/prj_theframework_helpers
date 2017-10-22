9a4b088b9a4e5b86079ee68def68b0daf90c8002
eacevedof <eacevedof@yahoo.es>
2017-10-22 17:20:08 +0200

    404

--- the_application/views/status/404.php
+++ the_application/views/status/404.php
@@ -1,4 +1,4 @@
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" rel="stylessheet"/>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 
 <style type="text/css">
 .error-template {padding: 40px 15px;text-align: center;}
