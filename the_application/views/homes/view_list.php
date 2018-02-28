758a35e96ebe60dddf80d770fa2574ba29cb63ff
Eduardo A.F <eacevedof@yahoo.es>
2018-02-28 23:08:09 +0100

    view_list 1.2.0

--- the_application/views/homes/view_list.php
+++ the_application/views/homes/view_list.php
@@ -1,5 +1,6 @@
-<!--view_list 1.1.0-->
+<!--view_list 1.2.0-->
 <div class="col-lg-12">
+    <span class="glyphicon glyphicon-envelope"></span>
     <br/>        
     <table class="table table-striped table-responsive table-hover">
         <thead>
@@ -8,27 +9,37 @@
         </tr>
         </thead>
         <tbody>
-    <?php
+<?php
+//http://glyph.smarticons.co/#usage
     $iRow=0;
-
+    //bug($arHelpers);
     foreach($arHelpers as $i=>$arHelper):
         $iRow++;
         $sSlug = $arHelper["slug"];
         $sUrl = "/$sSlug/examples/";
         $sUrlContent = "/$sSlug/";
-    ?>    
+?>    
         <tr>
             <th scope="row"><?php s($i);?></th>
             <td>
-                <a class="btn btn-default" href="<?php s($sUrl);?>" role="button"><?php s($arHelper["classname"]);?></a>
+                <a class="btn btn-default" href="<?php s($sUrl);?>" role="button">
+                    <?php s($arHelper["classname"]);?>
+<?php
+if($arHelper["has_example"]):
+?>
+                    <img src="/images/svg/si-glyph-checked.svg" height="10" width="10"/>
+<?php
+endif;
+?>                    
+                </a>
             </td>
             <td>
                 <a class="btn" href="<?php s($sUrlContent);?>" role="button"><?php s($arHelper["filename"]);?></a>
             </td>
         </tr>
-    <?php
+<?php
     endforeach;
-    ?>
+?>
         </tbody>
     </table> 
 </div>
