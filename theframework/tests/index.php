9da6d10bd520f73667863db7e7571f49d18eefd7
Eduardo A.F <eacevedof@yahoo.es>
2018-12-06 12:56:05 +0100

    index.php con hotreaload

--- theframework/tests/index.php
+++ theframework/tests/index.php
@@ -6,4 +6,17 @@ include_once "../helpers/autoload_nc.php";
 use \TheFramework\Helpers\Html\Div;
 
 $oDiv = new Div();
-$oDiv->set_innerhtml("hola");
+$oDiv->set_innerhtml("gato");
+$oDiv->show();
+
+?>
+<script type="text/javascript">
+var iThread = setTimeout("location.reload(true);",10000);
+console.log("iThread:",iThread)
+
+function resetTimeout(){
+    consolo.log("Removing thread:",iThread)
+    clearTimeout(iThread);
+    iThread = setTimeout("location.reload(true);",10000);
+}
+</script>
