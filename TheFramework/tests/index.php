e25d81c71a1659d987fc1a866a2cdd940ba8ea7b
Eduardo A.F <eacevedof@yahoo.es>
2018-12-06 09:00:26 +0100

    autoload no_cp todavia no funciona bien

--- TheFramework/tests/index.php
+++ TheFramework/tests/index.php
@@ -1,3 +1,8 @@
 <?php
-include_once "../components/autoload.php";
-include_once "../helpers/src/autoload.php";
+//include_once "../components/autoload.php";
+include_once "../helpers/autoload_nc.php";
+
+use \TheFramework\Helpers\Html\Div;
+
+$oDiv = new Div();
+$oDiv->set_inner_html("hola");
