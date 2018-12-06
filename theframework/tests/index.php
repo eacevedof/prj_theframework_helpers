8ca1b2ec2e83a128652606a886fe94120c8c7226
Eduardo A.F <eacevedof@yahoo.es>
2018-12-06 13:48:58 +0100

    checkbox ahora date

--- theframework/tests/index.php
+++ theframework/tests/index.php
@@ -8,8 +8,10 @@ use TheFramework\Helpers\Form\Input\Checkbox;
 //https://www.w3schools.com/tags/att_input_checked.asp
 $o = new Checkbox();
 $o->set_name("chkSome");
+$o->set_unlabeled(0);
 $o->set_options(["valbike"=>"Bike","valcar"=>"Car"]);
 $o->show();
+        
 ?>
 <!-- hot reload -->
 <meta http-equiv="refresh" content="5">
