85d7862c956ed4f2a9809d2c6c780a6ad1143001
Ubuntu docker <eacevedof@yahoo.es>
2018-12-06 16:57:43 +0000

    borrado TheFramework

--- TheFramework/tests/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-//include_once "../components/autoload.php";
-include_once "../helpers/autoload_nc.php";
-
-use \TheFramework\Helpers\Html\Div;
-
-$oDiv = new Div();
-$oDiv->set_inner_html("hola");
