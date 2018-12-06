52da7e686387bc1d09bac4bdc3035881eabbebc6
Eduardo A.F <eacevedof@yahoo.es>
2018-12-06 12:44:12 +0100

    index.php

---
+++ theframework/tests/index.php
@@ -0,0 +1,9 @@
+<?php
+//index.php
+//include_once "../components/autoload.php";
+include_once "../helpers/autoload_nc.php";
+
+use \TheFramework\Helpers\Html\Div;
+
+$oDiv = new Div();
+$oDiv->set_inner_html("hola");
