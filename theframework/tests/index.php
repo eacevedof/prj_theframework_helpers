f0da482caf1eb6862120fbe7c480c5c88ea58dc9
Eduardo A.F <eacevedof@yahoo.es>
2018-12-06 13:00:55 +0100

    hot reload

--- theframework/tests/index.php
+++ theframework/tests/index.php
@@ -8,19 +8,5 @@ use \TheFramework\Helpers\Html\Div;
 $oDiv = new Div();
 $oDiv->set_innerhtml("gato");
 $oDiv->show();
-
 ?>
-<script type="text/javascript">
-//fuente: https://stackoverflow.com/questions/4644027/how-to-automatically-reload-a-page-after-a-given-period-of-inactivity
-var iTimeSec = 10
-
-iTimeSec = iTimeSec * (1000)
-var iThread = setTimeout("location.reload(true);",iTimeSec)
-console.log("iThread:",iThread)
-
-function resetTimeout(){
-    consolo.log("Removing thread:",iThread)
-    clearTimeout(iThread);
-    iThread = setTimeout("location.reload(true);",iTimeSec)
-}//resetTimeout
-</script>
+<meta http-equiv="refresh" content="5">
