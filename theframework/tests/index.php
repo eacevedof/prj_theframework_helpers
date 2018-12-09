fd25f121f368a91bb158d451b4ef14c6f23761eb
Eduardo A.F <eacevedof@yahoo.es>
2018-12-09 13:23:42 +0100

    use TheFramework\Helpers\Html\Table\Tr; use TheFramework\Helpers\Html\Table\Td; use TheFramework\Helpers\Html\Table\Table;

--- theframework/tests/index.php
+++ theframework/tests/index.php
@@ -1,6 +1,7 @@
 <!-- hot reload -->
 <meta http-equiv="refresh" content="5">
 <a href="https://github.com/eacevedof/prj_theframework_helpers/tree/develop" target="_blank">Repo</a>
+<a href="https://github.com/eacevedof/prj_theframework_helpers/blob/develop/theframework/helpers/EXAMPLES.md" target="_blank">Examples</a>
 <pre>php -S localhost:3000 -t tests</pre>
 <?php
 //index.php
