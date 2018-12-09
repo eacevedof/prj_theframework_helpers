60de44db3fd46d5f9044511f5b27d8a4d17cdd3e
Eduardo A.F <eacevedof@yahoo.es>
2018-12-09 13:22:37 +0100

    use TheFramework\Helpers\Html\Table\Tr; use TheFramework\Helpers\Html\Table\Td; use TheFramework\Helpers\Html\Table\Table;

--- theframework/tests/index.php
+++ theframework/tests/index.php
@@ -1,5 +1,7 @@
 <!-- hot reload -->
 <meta http-equiv="refresh" content="5">
+<a href="https://github.com/eacevedof/prj_theframework_helpers/tree/develop" target="_blank">Repo</a>
+<pre>php -S localhost:3000 -t tests</pre>
 <?php
 //index.php
 //include_once "../components/autoload.php";
