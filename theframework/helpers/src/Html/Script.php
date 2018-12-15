eb3daa4b7fdebeae15cac3ac70ac1d1dc27ae163
Eduardo A.F <eacevedof@yahoo.es>
2018-12-15 11:29:16 +0100

    use TheFramework\Helpers\Html\Script

--- theframework/helpers/src/Html/Script.php
+++ theframework/helpers/src/Html/Script.php
@@ -29,40 +29,66 @@ class Script extends TheFrameworkHelper
     public function get_opentag() 
     {
         $arOpenTag[] = "<$this->_tag";
-        if($this->_id) $arOpenTag[] = " id=\"$this->_idprefix$this->_id\"";           
+        if($this->_id) $arOpenTag[] = " id=\"$this->_idprefix$this->_id\"";
         if($this->arExtras) $arOpenTag[] = " ".$this->get_extras();
         $arOpenTag[] = ">\n";
         return implode("",$arOpenTag);
     }//get_opentag
 
-
-    public function get_closetag(){return "</{$this->_tag}>";}//get_closetag
+    public function get_closetag(){return "\n</{$this->_tag}>";}//get_closetag
 
     public function get_html()
     {  
         $arHtml[] = $this->get_opentag();
-        $this->load_inner_objects();
+        $this->load_inner_objects("\n");
         $arHtml[] = $this->_inner_html;
         $arHtml[] = $this->get_closetag();
         return implode("",$arHtml);
-
     }//get_html
     
-
+    public function get_htmlsrc()
+    {
+        //pr($this->arSrc);die;
+        $arHtml = [];
+        foreach($this->arSrc as $mxSrc)
+        {
+            $arTmp = [];       
+            if($this->_type) 
+                $arTmp[] = "type=\"{$this->_type}\"";
+                
+            if(is_string($mxSrc))
+            {
+                $arTmp[] = "src=\"$mxSrc\"";
+                $arHtml[] = "<script ".implode(" ",$arTmp)."></script>";
+            }
+            elseif(is_array($mxSrc))
+            {
+                foreach($mxSrc as $k=>$v)
+                    $arTmp[] = "$k=\"$v\"";
+                $arHtml[] = "<script ".implode(" ",$arTmp)."></script>";
+            }
+        }//foreach arSrc
+        return implode("\n",$arHtml);
+        
+    }//get_htmlsrc
+    
     //**********************************
     //             SETS
     //**********************************
-
+    public function set_src($arSrc=[]){$this->arSrc = $arSrc;}    
+    public function add_src($sSrc){$this->arSrc[] = $sSrc;}
+    public function add_srcext($sSrc,$arExtra=[]){$this->arSrc[] = array_merge(["src"=>$sSrc],$arExtra);}
  
     //**********************************
     //             GETS
     //**********************************
-   
+    
 
     //**********************************
     //           MAKE PUBLIC
     //**********************************
     public function show_opentag(){echo $this->get_opentag();}
     public function show_closetag(){echo $this->get_closetag();}
- 
+    public function show_htmlsrc(){echo $this->get_htmlsrc();}
+    
 }//Script
