2e4525c0c522517cce44919a2914f2ff294c9a9f
Eduardo A.F <eacevedof@yahoo.es>
2018-12-16 19:01:36 +0100

    Nuevo GoogleMaps

---
+++ theframework/helpers/src/Vendor/Google/GoogleMaps.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @author Eduardo Acevedo Farje.
+ * @link www.eduardoaf.com
+ * @version 1.0.0
+ * @name TheFramework\Helpers\Vendor\GoogleMaps  
+ * @date 16-12-2018 17:53
+ * @file GoogleMaps.php
+ */
+namespace TheFramework\Helpers\Vendor\Google;
+
+class GoogleMaps
+{
+    public function __construct() {
+        ;
+    }
+    
+}//GoogleMaps
--- theframework/tests/index.php
+++ theframework/tests/index.php
@@ -1,5 +1,5 @@
 <!-- hot reload -->
-<meta http-equiv="refresh" content="5">
+<meta http-equiv="refresh" content="500">
 <a href="https://github.com/eacevedof/prj_theframework_helpers/tree/develop" target="_blank">Repo</a>
 <a href="https://github.com/eacevedof/prj_theframework_helpers/blob/develop/theframework/helpers/EXAMPLES.md" target="_blank">Examples</a>
 <pre>php -S localhost:3000 -t tests</pre>
@@ -9,7 +9,7 @@
 include_once "../helpers/autoload_nc.php";
 
 use TheFramework\Helpers\Html\Script;
-use TheFramework\Helpers\Vendor\GoogleMaps3;
+use TheFramework\Helpers\Vendor\Google\GoogleMaps;
 
 $oScript = new Script();
 $sPathFrom = realpath(__DIR__."/../helpers/src/Vendor/Google/GoogleMaps3.js");
@@ -19,26 +19,43 @@ $oScript->add_public($sPathFrom,$sPathTo);
 $oScript->move_topublic();
 $oScript->add_src("/js/GoogleMaps3.js");
 
+$oGoogleMap = new GoogleMaps();
 
-
-$oGoogleMap3 = new GoogleMaps3($arTrayecto);
-$oGoogleMap3->set_zoom(2);
-//$oGoogleMap3->get_latlong_from_address(array("pais"=>"Espa�a","direccion"=>"Conde de Pe�alver 32", "zona"=>"Madrid", "cp"=>"28006"));
-/**/
-//bug($oGoogleMap3->get_url_signed("http://maps.googleapis.com/maps/api/js?sensor=false&client=$config_google_clientid", $config_google_cryptokey));
-//devuelve : http://maps.googleapis.com/maps/api/js?sensor=false&client=gme-telynetsa&signature=6mBkaXZ7zfF9Je5rviTegiys-mA=
-$oGoogleMap3->set_client_id($config_google_clientid);
-$oGoogleMap3->set_channel("adeccorot");
-/**/
-//$oGoogleMap3->draw_lines();
-$oGoogleMap3->set_size_container(795,475);
-$oGoogleMap3->no_google_jquery();
-$oGoogleMap3->draw_routes();
-
-//SE DIBUJA EL MAPA
-$oGoogleMap3->draw_map();
 ?>
-<div id="divMap"></div>
-<?php
-$oScript->show_htmlsrc();
-?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <style>
+       /* Set the size of the div element that contains the map */
+      #map {
+        height: 400px;  /* The height is 400 pixels */
+        width: 100%;  /* The width is the width of the web page */
+       }
+    </style>
+  </head>
+  <body>
+    <h3>My Google Maps Demo</h3>
+    <!--The div element for the map -->
+    <div id="map"></div>
+    <script>
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
+    </script>
+    <!--Load the API from the specified URL
+    * The async attribute allows the browser to render the page while the API loads
+    * The key parameter will contain your own API key (which is not needed for this tutorial)
+    * The callback parameter executes the initMap() function
+    -->
+    <script async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDjgTpYTwwgJtthbRh3vYZUS1xkpC-bf0k&callback=initMap">
+    </script>
+  </body>
+</html>
