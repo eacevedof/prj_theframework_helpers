989e43deb357cafdd1817e0283601ec6c3d97d73
eacevedof <eacevedof@yahoo.es>
2018-12-18 16:41:08 +0100

    mapas

--- theframework/helpers/src/Vendor/Google/GoogleMaps.php
+++ theframework/helpers/src/Vendor/Google/GoogleMaps.php
@@ -16,6 +16,8 @@ class GoogleMaps
     private $sDivId;
     private $sDivWidth;
     private $sDivHeight;
+
+    private $arMarkers;
     
     public function __construct($sApikey="") 
     {
@@ -49,16 +51,41 @@ class GoogleMaps
     <!--The div element for the map -->
     <div id="<?= $this->sDivId ?>"></div>
     <script>
+    //https://stackoverflow.com/questions/3059044/google-maps-js-api-v3-simple-multiple-marker-example
         // Initialize and add the map
         function initMap()
         {
-            // The location of Uluru
-            var uluru = {lat: -25.344, lng: 131.036};
-            // The map, centered at Uluru
-            var map = new google.maps.Map(
-              document.getElementById('map'), {zoom: 4, center: uluru});
-            // The marker, positioned at Uluru
-            var marker = new google.maps.Marker({position: uluru, map: map});
+            var locations = [
+      ['Bondi Beach', -33.890542, 151.274856, 4],
+      ['Coogee Beach', -33.923036, 151.259052, 5],
+      ['Cronulla Beach', -34.028249, 151.157507, 3],
+      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+      ['Maroubra Beach', -33.950198, 151.259302, 1]
+    ];
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: new google.maps.LatLng(-33.92, 151.25),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+    }
         }
     </script>
     <!--Load the API from the specified URL
@@ -71,4 +98,6 @@ class GoogleMaps
     }//show_div
     
     public function set_aplikey($sKey){$this->sApikey=$sKey;}
+    public function add_marker($sLat,$sLong,$sText=""){$this->arMarkers[]=["text"=>$sText,"lat"=>$sLat,"long"=>$sLong];}
+
 }//GoogleMaps
