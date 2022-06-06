addcee3af41c13b15c1719d3e175b7a1dbd6e029
ioedumac <eacevedof@yahoo.es>
2022-06-06 21:09:48 +0200

    refactor form

--- theframework/helpers/src/Form/Fieldset.php
+++ theframework/helpers/src/Form/Fieldset.php
@@ -11,35 +11,39 @@
 namespace TheFramework\Helpers\Form;
 use TheFramework\Helpers\AbsHelper;
 
-class Fieldset extends AbsHelper
+final class Fieldset extends AbsHelper
 {
+    private const TYPE = "fieldset";
     
-    public function __construct($innerhtml="", $id="", $class="", $style="", $extras=[])
-    {
-        $this->type = "fieldset";
-        $this->idprefix = "";
-        $this->id = $id;
+    public function __construct(
+        string $innerhtml="", 
+        string $id="", 
+        string $class="", 
+        string $style="", 
+        array $extras=[]
+    ) {
         
-        $this->innerhtml = $innerhtml;
-        if($class) $this->arclasses[] = $class;
-        if($style) $this->arStyles[] = $style;
-        $this->extras = $extras;
+        $this
+            ->id($id)
+            ->innerhtml($innerhtml)
+            ->class($class)
+            ->style($style)
+            ->extras($extras)
+        ;
     }
-    
-    //Fieldset
-    public function get_html()
+
+    public function get_html(): string
     {  
-        $arHtml = [];
-        if($this->comment) $arHtml[] = "<!-- $this->comment -->\n";
-        $arHtml[] = $this->get_opentag(); 
-        //Agrega a inner_html los valores obtenidos con get_html de cada objeto
+        $arhtml = [];
+        if($this->comment) $arhtml[] = "<!-- $this->comment -->\n";
+        $arhtml[] = $this->get_opentag();
         $this->_load_inner_objects();
-        $arHtml[] = $this->innerhtml;
-        $arHtml[] = $this->get_closetag();
-        return implode("",$arHtml);
+        $arhtml[] = $this->innerhtml;
+        $arhtml[] = $this->get_closetag();
+        return implode("",$arhtml);
     }//get_html
         
-    public function get_opentag()
+    public function get_opentag(): string
     {
         $arOpenTag[] = "<$this->type";
         if($this->id) $arOpenTag[] = " id=\"$this->idprefix$this->id\"";
@@ -56,29 +60,8 @@ class Fieldset extends AbsHelper
         if($this->class) $arOpenTag[] = " class=\"$this->class\"";
         $this->_load_style();
         if($this->style) $arOpenTag[] = " style=\"$this->style\"";
-        //atributos extra
-        if($this->_attr_dbfield) $arOpenTag[] = " dbfield=\"$this->_attr_dbfield\"";
-        if($this->_attr_dbtype) $arOpenTag[] = " dbtype=\"$this->_attr_dbtype\"";              
         if($this->extras) $arOpenTag[] = " ".$this->get_extras();
-        //if($this->_isPrimaryKey) $arOpenTag[] = " pk=\"pk\"";
-        //if($this->_attr_dbtype) $arOpenTag[] = " dbtype=\"$this->_attr_dbtype\"";  
         $arOpenTag[] =">";        
         return implode("",$arOpenTag);        
-    }//get_opentag  
-        
-    //**********************************
-    //             SETS
-    //**********************************
-
-    
-    //**********************************
-    //             GETS
-    //**********************************
-    
-    //**********************************
-    //           MAKE PUBLIC
-    //**********************************
-    public function show_opentag(){parent::show_opentag();}
-    public function show_closetag(){parent::show_closetag();}
-    
-}//Fieldset
+    }
+}
